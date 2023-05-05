5b535cfeb4c513c7cd10fb7875fbb26ba9ed0404
Vishnuvardhan Chellamuthu <vishnu@192.168.0.104>
2023-05-05 22:43:13 +0530

    node

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ let paymentSettlement;
 
 app.post("/customer_details/:name", (req, res) => {
   paymentSettlement = req.body;
-  res.redirect("https://sri-abirami-finance-frontend.onrender.com/customer_details/:name");
+  res.redirect("/customer_details/:name");
 })
 
 //, Status: paymentStatus
@@ -286,7 +286,7 @@ app.get("/customer_details/:name", function (req, res) {
         res.status(500).send('Error searching for users');
       })
   }
-  res.redirect("https://sri-abirami-finance-frontend.onrender.com/customer_details/:name");
+  // res.redirect("/customer_details/:name");
 
 });
 
@@ -463,7 +463,7 @@ app.post("/cust_update/:cardno", (req, res) => {
     });
   }
 
-  res.redirect("https://sri-abirami-finance-frontend.onrender.com/cust_update/:cardno");
+  res.redirect("/cust_update/:cardno");
 })
 
 
@@ -499,8 +499,6 @@ app.get("/cust_update/:cardno", (req, res) => {
       console.log(err);
       res.status(500).send('Error searching for users');
     })
-
-    res.redirect("https://sri-abirami-finance-frontend.onrender.com/cust_update/:cardno")
 })
 
 app.post("/interest_update", function (req, res) {
@@ -524,7 +522,7 @@ app.post("/interest_update", function (req, res) {
   // console.log(interestReceivedDate);
   // console.log(interestCustCardNo);
 
-  res.redirect(`https://sri-abirami-finance-frontend.onrender.com/cust_update/${interestCustCardNo}`);
+  res.redirect(`/cust_update/${interestCustCardNo}`);
 
 })
 
