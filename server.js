9fe866b57dfc38572b55a38a8ffa1a3f1da59e2d
Vishnuvardhan Chellamuthu <vishnu@192.168.1.105>
2023-05-10 01:44:33 +0530

    node

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const { spawn } = require("child_process");
 
 const app = express();
 
-// app.use(express.static(path.join(__dirname, "../client/public")))
+// app.use(express.static(path.join(__dirname, "../client/public")));
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../client/public", "index.html"));
